5d6abde368186ba01ae0d355828923c041cd4cef
Mittenover <lucien.perdrix@gmail.com>
2022-05-03 15:54:55 +0200

    Avancées dans la création de l'environnement

---
+++ Snake/gameEnv.c
@@ -0,0 +1,32 @@
+#include "gameEnv.h" 
+#include "Qlearning.h"
+
+void alloc_grid(){  //On alloue dynamiquement un tableau carré de caractère de la taille de la dimension
+     grid = malloc(dim * sizeof(char*));
+
+     for(int i=0; i<dim; i++) {
+         grid[i] = malloc(dim * sizeof(char*));
+     }
+}
+
+void grid_make(int size){
+	for(int i=0;i<size;++i){
+		grid[i][0]='+';
+		grid[0][i]='+';
+		grid[size-1][i]='+';
+		grid[0][size-1]='+';
+	}
+}
+
+void grid_render(){
+     for (int i=0; i<dim; i++) {
+         for (int j=0; j< dim; j++){
+             printf("%c ", grid[i][j]);
+         }
+         printf("\n");
+     }
+     printf("\n");
+}
+
+
+     
---
+++ Snake/gameEnv.h
@@ -0,0 +1,25 @@
+#ifndef GAMEENV_H
+#define GAMEENV_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+char** grid;
+int** visited;
+int dim;
+int start_row;
+int start_col;
+int state_row;
+int state_col;
+int goal_row;
+int goal_col;
+
+void alloc_grid();
+
+void grid_make(char* );
+
+void grid_render(); 
+
+
+#endif
