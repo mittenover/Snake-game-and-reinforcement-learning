04907648f80e4147c2009ddb5561c0686da9f900
Mittenover <lucien.perdrix@gmail.com>
2022-05-10 15:27:44 +0200

    création de la fonction faisant spawn les fruit dans la grille

--- Snake/gameEnv.c
+++ Snake/gameEnv.c
@@ -9,12 +9,12 @@ void alloc_grid(){  //On alloue dynamiquement un tableau carré de caractère de
      }
 }
 
-void grid_make(int dim){
-	for(int i=0;i<dim;++i){
+void grid_make(int size){
+	for(int i=0;i<size;++i){
 		grid[i][0]='+';
 		grid[0][i]='+';
-		grid[dim-1][i]='+';
-		grid[0][dim-1]='+';
+		grid[size-1][i]='+';
+		grid[0][size-1]='+';
 	}
 }
 
@@ -32,6 +32,12 @@ void grid_render(){
 void new_fruit(){
 	int i=rand()%dim;
 	int j=rand()%dim;
-	
+	int k=0;
+	while((grid[i][j]==wall)&&(k<100*size**2)){
+		i=rand()%dim;
+		j=rand()%dim;
+		k+=1;
+	}
+	grid[i][j]='°';
 
 }
